fc65e870edabae21145b38a5087f65b406b4601f
salzbrezel <h.2.o@hotmail.de>
2013-03-21 13:26:00 +0100

    portable zeitmessung

--- fraktal.c
+++ fraktal.c
@@ -10,15 +10,35 @@
 
 #include "fraktal.h"
 #include "graphic.h"
-#include <windows.h> //reference timer
+
 #include <xmmintrin.h> //sse intrinsics
-double my_get_time()
+
+#if defined _WIN64 || defined _WIN32
+
+#include <windows.h>
+double get_time()
 {
     LARGE_INTEGER t, f;
     QueryPerformanceCounter(&t);
     QueryPerformanceFrequency(&f);
     return (double)t.QuadPart/(double)f.QuadPart;
 }
+
+#else
+
+#include <sys/time.h>
+#include <sys/resource.h>
+
+double get_time()
+{
+    struct timeval t;
+    struct timezone tzp;
+    gettimeofday(&t, &tzp);
+    return t.tv_sec + t.tv_usec*1e-6;
+}
+
+#endif
+
 
 
 double getRadius(struct tComplex z)
